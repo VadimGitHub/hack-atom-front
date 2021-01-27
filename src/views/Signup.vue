953d124bb2a228b85763c7ac825bbdb9ddfa674e
chvss.era <chvss.era@gmail.com>
2021-01-27 12:32:20 +0300

    add signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,15 +1,66 @@
 <template>
-  <div>
-    <h2>Регистрация</h2>
+  <!-- Root element for center items -->
+  <div class="flex flex-col bg-gray-100 mt-52">
+    <!-- Auth Card Container -->
+    <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <!-- Auth Card -->
+      <div class="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12
+            px-6 py-10 sm:px-10 sm:py-6
+            bg-white rounded-lg shadow-md lg:shadow-lg">
+
+        <!-- Card Title -->
+        <h2 class="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
+          Регистрация
+        </h2>
+
+        <form class="mt-10" method="POST">
+          <!-- Email Input -->
+          <label for="email" class="block text-xs font-semibold text-gray-600 uppercase">E-mail</label>
+          <input id="email" type="email" name="email" placeholder="e-mail" autocomplete="email"
+                 class="block w-full py-3 px-1 mt-2
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                 required/>
+
+          <label for="company" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Предприятие</label>
+          <input id="company" type="text" name="company" placeholder="предприятие" autocomplete="company"
+                 class="block w-full py-3 px-1 mt-2 mb-4
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                 required/>
+
+          <!-- Password Input -->
+          <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Пароль</label>
+          <input id="password" type="password" name="password" placeholder="Пароль" autocomplete="current-password"
+                 class="block w-full py-3 px-1 mt-2 mb-4
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                 required/>
+
+          <button type="submit"
+                  class="w-full py-3 mt-10 bg-gray-800 rounded-sm
+                    font-medium text-white uppercase
+                    focus:outline-none hover:bg-gray-700 hover:shadow-none">
+            Зарегистрироваться
+          </button>
+
+          <!-- Another Auth Routes -->
+          <div class="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-right">
+            <router-link to="/login" class="flex-2 underline">
+              Есть аккаунт?
+            </router-link>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "Register"
+  name: "Signup"
 }
 </script>
-
-<style scoped>
-
-</style>
