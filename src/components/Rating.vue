c63d0ea2771b9dfb5b15ed92fdaeb1023eddd01f
Alex-Parshin <parshin9696@mail.ru>
2021-02-05 18:53:20 +0300

    рейтинг тест

---
+++ src/components/Rating.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="box">
+    <div class="rating">
+      <div class="rating"><span v-for="i in 5" :key="i" :style="6-i<=one?'color:red;':'color:#888'" @click="one=6-i">☆</span></div>
+      <div class="rating"><span v-for="i in 5" :key="i" :style="6-i<=two?'color:red;':'color:#888'" @click="two=6-i">☆</span></div>
+      <div class="rating"><span v-for="i in 5" :key="i" :style="6-i<=three?'color:red;':'color:#888'" @click="three=6-i">☆</span></div>
+    </div>
+
+    <h1 class="text-5xl">{{ rate }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rating",
+  data() {
+    return {
+      one: 0,
+      two: 0,
+      three: 0,
+
+    }
+  },
+  computed: {
+    rate() {
+      return Number(this.one) + Number(this.two) + Number(this.three);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  width: 300px ;
+  display: flex;
+  flex-direction: column;
+}
+</style>
