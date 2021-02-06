d5ea2a972db2c123e018c83220bddc6659207081
chvss <chvss.era@gmail.com>
2021-02-06 15:35:46 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import './index.css'
 
 // API_URL=http://10.0.0.108:8080/
 // API_URL=http://78.142.222.206:8080/api/
-Axios.defaults.baseURL = 'http://10.0.0.108:8080/api';
+Axios.defaults.baseURL = 'http://10.0.0.108:8080/api/';
 
 const token = localStorage.getItem('token')
 if (token) {
